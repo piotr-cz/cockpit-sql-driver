1201991823f8fba85f5af89f4de05f9e27a3cd56
Piotr Konieczny <hello@piotr.cz>
2020-02-10 15:48:18 +0100

    Added wrapper for simple storage

--- bootstrap.php
+++ bootstrap.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 namespace SqlDriver;
 
 use MongoHybrid\ClientWrapper as MongoHybridClientWrapper;
+use SimpleStorage\ClientWrapper as SimpleStorageClientWrapper;
 
 use MongoSql\Driver\Driver;
 
@@ -11,35 +12,61 @@ use MongoSql\Driver\Driver;
 require_once __DIR__ . '/autoload.php';
 
 $dbConfig = $app->retrieve('config/database');
+$memoryConfig = $app->retrieve('config/memory');
 
-// Skip when server other than sqldriver
-if ($dbConfig['server'] !== Driver::SERVER_NAME) {
-    return;
+// Skip when database server other than sqldriver
+if ($dbConfig['server'] === Driver::SERVER_NAME) {
+    /**
+     * Register on bootstrap
+     * There may be a problem when native cockpit modules try to access storage before it's been overloaded
+     * For example modules/Cockpit/bootstrap.php -> webhooks.php
+     *
+     * @var \LimeExtra\App $this
+     * @var \LimeExtra\App $app
+     * @var \Lime\Module $module
+     *
+     * Note: classes may be autoloaded after app has booted which happens after module is booted
+     */
+    $app->on('cockpit.bootstrap', function () use ($dbConfig): ?bool {
+        // Overwrite storage in registry
+        $this->set('storage', function () use ($dbConfig): MongoHybridClientWrapper {
+            static $client = null;
+
+            if ($client === null) {
+                $client = new MongoHybridClientWrapper(
+                    $dbConfig['server'],
+                    $dbConfig['options'],
+                    $dbConfig['driverOptions']
+                );
+            }
+
+            return $client;
+        });
+
+        return true;
+    }, $dbConfig['options']['bootstrapPriority'] ?? 1);
 }
 
-/**
- * Register on bootstrap
- * @var \LimeExtra\App $this
- * @var \LimeExtra\App $app
- * @var \Lime\Module $module
- *
- * Note: classes may be autoloaded after app has booted which happens after module is booted
- */
-$app->on('cockpit.bootstrap', function () use ($dbConfig): ?bool {
-    // Overwrite storage in registry
-    $this->set('storage', function () use ($dbConfig): MongoHybridClientWrapper {
-        static $client = null;
+// Skip when memory server oher than sqldriver
+if ($memoryConfig['server'] === Driver::SERVER_NAME) {
+    /**
+     * Register on bootsttap
+     */
+    $app->on('cockpit.bootstrap', function () use ($memoryConfig): ?bool {
+        $this->set('memory', function () use ($memoryConfig): SimpleStorageClientWrapper {
+            static $client = null;
 
-        if ($client === null) {
-            $client = new MongoHybridClientWrapper(
-                $dbConfig['server'],
-                $dbConfig['options'],
-                $dbConfig['driverOptions']
-            );
-        }
+            if ($client === null) {
+                $client = new SimpleStorageClientWrapper(
+                    $memoryConfig['server'],
+                    $memoryConfig['options'],
+                    $memoryConfig['driverOptions'] ?? []
+                );
+            }
 
-        return $client;
-    });
+            return $client;
+        });
 
-    return true;
-}, $dbConfig['options']['bootstrapPriority'] ?? 1);
+        return true;
+    }, $memoryConfig['options']['bootstrapPriority'] ?? 1);
+}
---
+++ lib/SimpleStorage/ClientWrapper.php
@@ -0,0 +1,89 @@
+<?php
+declare(strict_types=1);
+
+namespace SimpleStorage;
+
+use SimpleStorage\Client as SimpleStorageClient;
+
+use MongoSql\Driver\Driver;
+use MongoSql\DriverException;
+
+/**
+ * Monkey patched SimpleStorage\Client
+ *
+ * Note: Table schema different than in collections:
+ * id, document != key, keyval
+ *
+ * @see https://github.com/phpredis/phpredis/#keys
+ */
+class ClientWrapper extends SimpleStorageClient
+{
+    /** @var string - Collection/ Table name */
+    protected $collectionName = 'memory';
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct(string $server, array $options = [], array $driverOptions = [])
+    {
+        // Fall back to Cockpit drivers client
+        if ($server !== Driver::SERVER_NAME) {
+            parent::__construct($server, $options);
+
+            if (!$this->driver) {
+                throw new DriverException(sprintf('Could not initialize driver %s', $server));
+            }
+
+            return;
+        }
+
+        // Resolve drivers' FQCN
+        $fqcn = sprintf('MongoSql\Driver\%sDriver', ucfirst($options['connection']));
+
+        if (!class_exists($fqcn)) {
+            throw new DriverException(sprintf('SQL driver for %s not found', $options['connection']));
+        }
+
+
+        // Create new driver
+        $this->driver = new $fqcn($options, $driverOptions);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function get($key, $default = null): ?array
+    {
+        $val = $this->driver->findOne($this->collectionName, ['key' => $key]);
+
+        if ($val === null) {
+            return $default;
+        }
+
+        return $val['keyval'] ?? $default;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function set($key, $value): void
+    {
+        $doc = ['key' => $key, 'keyval' => $value];
+
+        // Does not exist
+        if ($this->get($key) === null) {
+            $this->driver->insert($this->collectionName, $doc);
+            return;
+        }
+
+        $this->driver->update($this->collectionName, ['key' => $key], $doc);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function del($key): void
+    {
+        $this->driver->remove($this->collectionName, ['key' => $key]);
+    }
+}
